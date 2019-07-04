8fca3dd2f88f5513e9273e08f70677cd729582aa
stevekutz <stkutz@gmail.com>
2019-07-04 17:29:40 -0500

    experimenting with responsive design

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -2,5 +2,9 @@ import React from "react";
 
 export default function(props) {
    console.log("Home props ", props);
-  return <h3> Landing Page</h3>;
+   return <h3 style = {{
+    border: '1px solid red',
+    width: "100%",
+    margin: "0 auto"
+    }} > Landing Page </h3>;
 }
