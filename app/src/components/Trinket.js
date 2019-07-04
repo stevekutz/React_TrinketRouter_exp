61efe2cd65af858616f3c3a42d53df673baa9296
stevekutz <stkutz@gmail.com>
2019-07-04 18:08:44 -0500

    made image on trinket page responsive

--- app/src/components/Trinket.js
+++ app/src/components/Trinket.js
@@ -12,16 +12,17 @@ export default function(props) {
     <div className="item-wrapper">
       <div className="item-header">
         
-        <div className="item-title-wrapper">
-          <h2>{item.name}</h2>
-          <h4>${item.price}</h4>
-        </div>
+          <div className="item-title-wrapper">
+            <h2>{item.name}</h2>
+            <h4>${item.price}</h4>
+          </div>
 
-        <div className="image-wrapper">
-          <img className = "imageItem" src={item.imageUrl} alt={item.name} />
-        </div>
+          <div className="image-wrapper">
+            <img className = "imageItem" src={item.imageUrl} alt={item.name} />
+          </div>
        
       </div>
+      
       <p className="item-description">{item.description}</p>
     </div>
   );
